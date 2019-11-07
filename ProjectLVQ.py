9060f0e14eb72f4fdef00c092a3c2c5bef166680
Abhishek <32640258+frtug@users.noreply.github.com>
2019-11-07 23:29:43 +0530

    6th commit
    Lets be Sure

--- ProjectLVQ.py
+++ ProjectLVQ.py
@@ -7,7 +7,8 @@ import numpy as np
 iris=datasets.load_iris()
 x=iris.data
 y=iris.target
-x[:5]
+print(x[:5])
+print(y[:])
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.2)
