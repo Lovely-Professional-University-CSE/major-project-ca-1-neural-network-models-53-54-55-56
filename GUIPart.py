56436ecb737c3675bc3d36ca4fda4d2751c37fe2
Abhishek Sharma <abhishek11703182@gmail.com>
2019-11-11 03:51:19 +0530

    2nd last commit

--- GUIPart.py
+++ GUIPart.py
@@ -3,26 +3,74 @@
 """
 Created on Fri Nov  8 02:36:48 2019
 
-@author: abhishek
+@author: abhishek (Frtug)
 """
 import numpy as np
 import pandas as pd
 from neupy import algorithms
-import seaborn as sns
-sns.set_palette('husl')
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import pickle
-
 from  sklearn import  datasets
-import numpy as np
 
 from sklearn.metrics import accuracy_score
 
 from tkinter import *
 from tkinter import messagebox
 
+from tkinter import *
+
+# pip install pillow
+from PIL import Image, ImageTk
+
+
 classifier = algorithms.LVQ(n_inputs=4, n_classes=3)
+iris=datasets.load_iris()
+x=iris.data
+y=iris.target
+data = pd.DataFrame(iris.data)
+
+
+
+
+def Compare():
+    pass
+
+def Visual():
+    
+
+   # print(x[:5])
+   # print(y[:5])
+   # print(x.shape)
+    # print(y.head())
+    
+    #print(y.shape)
+    colors = (0,0,0)
+    
+    plt.scatter(data[0],y,c="red",alpha=0.5)
+    plt.title('Scatter plot Sepal length and Species')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    
+    plt.scatter(data[1], y, c="yellow", alpha=0.5)
+    plt.title('Scatter plot Sepal Width and Species')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    
+    plt.scatter(data[2], y, c="blue", alpha=0.5)
+    plt.title('Scatter plot petal length and Species')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    
+    plt.scatter(data[3], y, c='green', alpha=0.5)
+    plt.title('Scatter plot petal Width and Species')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
 
 def fuc():
     iris=datasets.load_iris()
@@ -47,6 +95,7 @@ def algorithms():
         E3 = float(e3.get())
         E4 = float(e4.get())
         u_input = np.array([E1,E2,E3,E4]).reshape(1,-1)
+        answer_label.config(text="Species Found!!!!",fg='red')
         print(u_input)
         classs = pickle.load(open('NEwQuerty.sav','rb'))
         print("KIS")
@@ -68,36 +117,38 @@ def algorithms():
 
     else:
           msg = messagebox.showinfo('Message','Enter the Values First')
+          answer_label.config(text="Enter the Length and Width",fg='green')
     print("EXIT")
 def fff():
   ##########################################################  
     roots.destroy()
     master = Tk()
     
-    master.geometry('400x300')
+    master.geometry('400x400')
+    global answer_label
     answer_label =Label(master, text ="---")
     answer_label.grid(row =10, column =0)
     
-    Label(master, text='Sepal_length').grid(row=0) 
-    Label(master, text='Sepal_width').grid(row=1)
-    Label(master, text='Petal_lenth').grid(row=2) 
-    Label(master, text='Petal_width').grid(row=3)
+    Label(master, text='Sepal_length',height=1, background='black', fg='white',font=("Cursive", 20)).grid(row=0) 
+    Label(master, text='Sepal_width',height=1, background='black', fg='white',font=("Cursive", 20)).grid(row=1)
+    Label(master, text='Petal_lenth',height=1, background='black', fg='white',font=("Cursive", 20)).grid(row=2) 
+    Label(master, text='Petal_width',height=1, background='black', fg='white',font=("Cursive", 20)).grid(row=3)
     global e1  
     global e2 
     global e3  
     global e4 
     
-    e1 = Entry(master) 
-    e2 = Entry(master)
-    e3 = Entry(master) 
-    e4 = Entry(master)
+    e1 = Entry(master,background='grey') 
+    e2 = Entry(master,background='grey')
+    e3 = Entry(master,background='grey') 
+    e4 = Entry(master,background='grey')
     
-    e1.grid(row=0, column=1) 
-    e2.grid(row=1, column=1)
-    e3.grid(row=2, column=1) 
-    e4.grid(row=3, column=1)
+    e1.grid(row=0, column=1,pady=10) 
+    e2.grid(row=1, column=1,pady=10)
+    e3.grid(row=2, column=1,pady=10) 
+    e4.grid(row=3, column=1,pady=10)
     
-    calculate_button =Button(master, text="Find", command= algorithms)
+    calculate_button =Button(master, text="FIND", command= algorithms)
     calculate_button.grid(row =7, column =0, columnspan =2)
     
     global status_label
@@ -107,32 +158,70 @@ def fff():
 
 ##################################################################    
 roots = Tk()
-t = roots.geometry('600x400')
+t = roots.geometry('1200x900')
 print(t)
 roots['bg'] = 'grey'
 roots.title('Details')
-intro = Label(roots, text='Project details', background='black', fg='white', font=("Bold", 40))
-intro.grid(row=0, sticky=N, padx=50, pady=10)
+intro = Label(roots, text='Project ',anchor="center", background='black', fg='white', font=("Bold", 70))
+intro.grid(sticky=N,padx=20,pady=20,columnspan=2)
 
-n1 = Label(roots, text=' Machine Learning: ', background='black', fg='white', font=("Cursive", 20))
-p1 = Label(roots, text=" Dataset: ", background='black', fg='white', font=("Cursive", 20))
-n1.grid(row=1, sticky=W, column=0, padx=20, pady=20)
-p1.grid(row=2, sticky=W, column=0, padx=20, pady=20)
 
-n2 = Entry(roots)
-p2 = Entry(roots)
-n2.insert(0, "a  value")
-p2.insert(0, "a value")
-n2.config(state = 'disabled')
-p2.config(state = 'disabled')
+n1 = Label(roots, text=' Machine Learning: ', background='black', fg='white', font=("Cursive", 40))
+p1 = Label(roots, text=" MODEL ", background='black', fg='white', font=("Cursive", 40))
+n1.grid(row=1, sticky=W, column=0, padx=10, pady=20)
+p1.grid(row=2, sticky=W, column=0, padx=10, pady=20)
+
+n2 = Label(roots, text=' IRIS DATASET ', background='black', fg='white', font=("Cursive", 40))
+p2 = Label(roots, text=" CLASSIFICATION: ", background='black', fg='white', font=("Cursive", 40))
+n2.grid(row=1, sticky=E, column=1, padx=10, pady=20)
+p2.grid(row=2, sticky=E, column=1, padx=10, pady=20)
+
+
+#n2.config(state = 'disabled')
+#p2.config(state = 'disabled')
 
 n2.grid(row=1, column=1, sticky=E, padx=20)
 p2.grid(row=2, column=1, sticky=E, padx=20)
 
-sigButton = Button(roots, text='TEST', command=fff)
-sigButton.grid(column=0, row=3, padx=0,columnspan=1)
+testButton = Button(roots, text='TEST',fg='red',width=7,height=2, command=fff)
+testButton.grid(column=2, row=1, padx=10,sticky=E)
 
-sigButton = Button(roots, text='TRAINING', command=fuc)
-sigButton.grid(column=0, row=3, padx=0,sticky=E)
+trainButton = Button(roots, text='TRAINING',fg='red',width=7,height=2, command=fuc)
+trainButton.grid(column=2, row=2, padx=10,sticky=E)
+
+visButton = Button(roots, text='VISUAL',fg='red',width=7,height=2, command=Visual)
+visButton.grid(column=3, row=1, padx=10,pady=10,sticky=E)
+
+compareButton = Button(roots, text='COMPARE',fg='red',width=7,height=2, command=Compare)
+compareButton.grid(column=3, row=2, padx=10,pady=10,sticky=E)
+
+setosalabel = Label(roots, text='SETOSA', background='black', fg='white', font=("Cursive", 20))
+setosalabel.grid(row = 3,column = 0)
+load = Image.open("Iris_setosa.jpg")
+render = ImageTk.PhotoImage(load)
+img = Label(image=render,height = 300,bg='blue')
+img.image = render
+
+img.grid(row = 10, column=0)
+
+
+versilabel = Label(roots, text='VERSICOLOR', background='black', fg='white', font=("aeril", 20))
+versilabel.grid(row = 3,column = 1)
+load = Image.open("Iris_Versicolor.jpg")
+render = ImageTk.PhotoImage(load)
+img = Label(image=render,bg='red')
+img.image = render
+img.grid(row = 10, column=1)  
+
+virglabel = Label(roots, text='VIRGINICA', background='black', fg='white', font=("Bold",20))
+virglabel.grid(row = 3,column = 2)
+load = Image.open("Iris_virginica.jpg")
+render = ImageTk.PhotoImage(load)
+img = Label(image=render,bg='green')
+img.image = render
+img.grid(row = 10, column=2)    
+
+
+
 roots.mainloop()
 
