77726891a1981ae90611cb263d72a58512b8cbd9
ANSHOO SINGH <anshoosingh36@gmail.com>
2020-02-16 23:17:53 +0530

    modified

--- Svm.py
+++ Svm.py
@@ -16,8 +16,8 @@ X=iris.data[:,[2,3]] #train
 print(X)
 y=iris.target 
 print(y)
-#plt.plot(X)
-#from sklearn.model_selection import train_test_split
+plt.plot(X)
+from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
 svc = sklearn.svm.SVC()
 svc.fit(X_train,y_train)
